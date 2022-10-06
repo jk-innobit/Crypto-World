8e056606f3fa88a8d497ec92b9295b9f5f7948a9
Jatin Kumar <jatin.kumar@innobitsystems.com>
2022-10-06 17:35:00 +0530

    add a new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes,Route,Link} from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 import './App.css';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar/>
+      </div>
+      <div className="main" style={{paddingLeft:'20px'}}>
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage/>}/>
+              <Route path='/exchanges' element={<Exchanges/>}/>
+              <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+              <Route path='/crypto/:coinId' element={<CryptoDetails/>}/>
+              <Route path='/news' element={<News/>}/>
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title level={5} style={{color:"white",textAlign:'center'}}>
+            Crypto-World <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to='/'>Home</Link>
+            <Link to='/exchanges'>Exchanges</Link>
+            <Link to='/news'>News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 }
